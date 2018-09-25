0273820b0f2b8ce867640d94f1446a2ccfe90269
ulloav <ulloav@VAL5306054>
2018-09-25 07:32:40 -0700

    finished assignment 1

---
+++ Assignment1/Assignment1.py
@@ -0,0 +1,17 @@
+"""
+    Vanessa Ulloa
+    CS21A
+    25 September 2018
+"""
+
+#   This prints the result of multiplying 234*456
+print("234 * 456 = ")
+print(234 * 456)
+
+#   Output
+"""
+234 * 456 = 
+106704
+
+Process finished with exit code 0
+"""
