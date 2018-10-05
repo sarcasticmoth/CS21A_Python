4e1ec3d58380e8c596e692c08105455b2c6e826a
Vanessa Ulloa <vanessa.e.ulloa@gmail.com>
2018-10-04 21:27:41 -0700

    init assignment 2

--- Assignment1/Assignment2.py
+++ Assignment1/Assignment2.py
@@ -5,6 +5,19 @@
 """
 
 #
+letter = 'Dear %s \nI would ike you to vote for %s\nbecause I think %s is best for\nthis country. \nSincerely, %s\n'
+firstLetter = ('Hildegard', 'Hillary Clinton', 'Hillary Clinton', 'Brunhilda')
+secondLetter = ('Cheech', 'Donald Trump', 'Donald Trump', 'Chong')
+thirdLetter = ('Moe', 'Bernie Sanders', 'Bernie Sanders', 'Jack')
+
+letterInputs = list(firstLetter)
+letterInputs.append(list(secondLetter))
+letterInputs.append(thirdLetter)
+
+print(letter % firstLetter)
+print(letterInputs[0])
+"""print(letter % letterInputs[1])
+print(letter % letterInputs[2])"""
 
 #   OUTPUT
 """
