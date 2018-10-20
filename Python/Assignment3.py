b251538c0f3bc21c4eeca7c409a7e3148ad0c82f
Vanessa Ulloa <vanessa.e.ulloa@gmail.com>
2018-10-20 10:29:58 -0700

    finished assignment 3

--- Python/Assignment3.py
+++ Python/Assignment3.py
@@ -2,14 +2,69 @@
     Vanessa Ulloa
     CS21A
     20 October 2018
+    Assignment 3
 """
 
-# Write a program that takes 3 different tuples with entries for adressee, candidate, sender
-# print 3 form letters using the tuple values.
+# this program contains several dictionaries that will be combined into a new dictionary with unique keys
+
+roommate1Shopping = {'fruit': 'apples', 'meat': 'chicken', 'vegetables': 'potatoes', 'drinks': ['beer','wine'], 'dessert': 'ice cream'}
+roommate2Shopping = {'fruit': 'lemons', 'meat': 'hamburger', 'drinks': ['apple juice', 'orange juice', 'vodka']}
+roommate3Shopping = {'fruit': ['oranges', 'bananas'], 'vegetables': ['lettuce', 'carrots'], 'drinks': 'milk'}
 
 
+# resulting dictionary
+mergedDict = roommate1Shopping.copy()
+
+# process the 2nd dictionary
+for key, value in roommate2Shopping.items():
+    if key not in mergedDict:
+        mergedDict[key] = value
+    else:
+        tempList = []
+        if isinstance(value, list):
+            tempList.extend(value)
+        else:
+            tempList.append(value)
+        oldValue = mergedDict[key]
+        if isinstance(oldValue, list):
+            tempList.extend(oldValue)
+        else:
+            tempList.append(oldValue)
+        mergedDict[key] = tempList
+
+# process the 3rd dictionary
+for key, value in roommate3Shopping.items():
+    if key not in mergedDict:
+        mergedDict[key] = value
+    else:
+        tempList = []
+        if isinstance(value, list):
+            tempList.extend(value)
+        else:
+            tempList.append(value)
+        oldValue = mergedDict[key]
+        if isinstance(oldValue, list):
+            tempList.extend(oldValue)
+        else:
+            tempList.append(oldValue)
+        mergedDict[key] = tempList
+
+print('original dictionaries:')
+print(roommate1Shopping)
+print(roommate2Shopping)
+print(roommate3Shopping)
+
+print('resulting dictionary:')
+print(mergedDict)
 
 #   OUTPUT
 """
+original dictionaries:
+{'fruit': 'apples', 'meat': 'chicken', 'vegetables': 'potatoes', 'drinks': ['beer', 'wine'], 'dessert': 'ice cream'}
+{'fruit': 'lemons', 'meat': 'hamburger', 'drinks': ['apple juice', 'orange juice', 'vodka']}
+{'fruit': ['oranges', 'bananas'], 'vegetables': ['lettuce', 'carrots'], 'drinks': 'milk'}
+resulting dictionary:
+{'fruit': ['oranges', 'bananas', 'lemons', 'apples'], 'meat': ['hamburger', 'chicken'], 'vegetables': ['lettuce', 'carrots', 'potatoes'], 'drinks': ['milk', 'apple juice', 'orange juice', 'vodka', 'beer', 'wine'], 'dessert': 'ice cream'}
 
+Process finished with exit code 0
 """
