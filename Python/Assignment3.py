e3a350828622d7c00946c45ad89f20d7d36cf7c9
Vanessa Ulloa <vanessa.e.ulloa@gmail.com>
2018-10-20 09:43:32 -0700

    add assignment 3

---
+++ Python/Assignment3.py
@@ -0,0 +1,15 @@
+"""
+    Vanessa Ulloa
+    CS21A
+    20 October 2018
+"""
+
+# Write a program that takes 3 different tuples with entries for adressee, candidate, sender
+# print 3 form letters using the tuple values.
+
+
+
+#   OUTPUT
+"""
+
+"""
