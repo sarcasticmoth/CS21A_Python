029b6fda4a27681c5bb5f4f22aa5cebd6e02f9af
ulloav <ulloav@VAL5306054>
2018-11-14 14:50:56 -0800

    start assignment 7

---
+++ Python/Assignment7/assignment7.py
@@ -0,0 +1,6 @@
+"""
+    Vanessa Ulloa
+    CS21A
+    14 November 2018
+    Assignment 7
+"""
---
+++ Python/Assignment7/employee.py
@@ -0,0 +1,7 @@
+"""
+    employee class
+    an object of this class represents one employee
+    that has a first name, last name, SSN and a salary
+"""
+
+class Employee:
