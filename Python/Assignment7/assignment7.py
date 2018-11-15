d50b37b6c63d65e6a9274a98400fe48070e7911f
ulloav <ulloav@VAL5306054>
2018-11-15 09:49:53 -0800

    finished assignment 7

--- Python/Assignment7/assignment7.py
+++ Python/Assignment7/assignment7.py
@@ -7,9 +7,45 @@
 
 # import employee and manager classes
 from employee import Employee
+from manager import Manager
 
 # this file is a test file to test the employee and manager classes
 
 if __name__ == "__main__":
-    vanessa = Employee("Vanessa", "Ulloa", "123456789", 10000)
-    print(vanessa)
+
+    companyEmployees = list()
+    companyEmployees.append(Manager("Jose", "Ulloa", "987-65-4321", 20000, "Manager", 2000))
+    companyEmployees.append(Manager("Vatche", "Alexan", "876-54-3210", 20000, "Manager", 2000))
+    companyEmployees.append(Employee("Vanessa", "Ulloa", "234-11-0973", 15000))
+    companyEmployees.append(Employee("Bob", "Wilson", "876-54-3210", 14000))
+    companyEmployees.append(Employee("John", "Doe", "423-41-2341", 13000))
+
+    # give a raise to every employee
+    # print the before raise and after raise
+    for e in companyEmployees:
+        print(e)
+        e.giveRaise(.10)
+        print(e)
+        print()
+
+    print("\nTests:\n")
+
+    # TEST: get the first name of every employee
+    print("Get Employee First Names:")
+    for e in companyEmployees:
+        print(e.getFirstName())
+
+    # TEST: get the last name of every employee
+    print("\nGet Employee Last Names:")
+    for e in companyEmployees:
+        print(e.getLastName())
+
+    # TEST: get the SSN of every employee
+    print("\nGet Employee SSN:")
+    for e in companyEmployees:
+        print(e.getSSN())
+
+    # TEST: get the salary of every employee
+    print("\nGet Employee Salaries:")
+    for e in companyEmployees:
+        print(e.getSalary())
--- Python/Assignment7/manager.py
+++ Python/Assignment7/manager.py
@@ -5,4 +5,23 @@
     this class inherits the employee class
 """
 
-from employee.py import employee
+# import Employee class that the Manager class will derive from
+from employee import Employee
+
+class Manager(Employee):
+    # constructor for Manager class
+    # inits the derived Employee class
+    def __init__(self, first, last, social, salary, title, bonus):
+        super().__init__(first, last, social, salary)
+        self.title = title
+        self.bonus = bonus
+
+    def getTitle(self):
+        return self.title
+
+    def getBonus(self):
+        return self.bonus
+
+    def __str__(self):
+        return "%s Title: %s and Bonus: $%i" % (super().__str__(), self.getTitle(), self.getBonus())
+
